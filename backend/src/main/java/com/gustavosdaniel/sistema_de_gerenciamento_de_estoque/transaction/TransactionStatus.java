ce1eae776c55a1d920f8ba322548efb6fd178e44
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-09-02 23:00:04 -0300

    feat: adicionandoo enuns userRole, transaction type e status

---
+++ backend/src/main/java/com/gustavosdaniel/sistema_de_gerenciamento_de_estoque/transaction/TransactionStatus.java
@@ -0,0 +1,8 @@
+package com.gustavosdaniel.sistema_de_gerenciamento_de_estoque.transaction;
+
+public enum TransactionStatus {
+
+    PENDING,
+    PROCESSING,
+    COMPLETED
+}
---
+++ backend/src/main/java/com/gustavosdaniel/sistema_de_gerenciamento_de_estoque/user/UserRole.java
@@ -0,0 +1,6 @@
+package com.gustavosdaniel.sistema_de_gerenciamento_de_estoque.user;
+
+public enum UserRole {
+    ADMIN,
+    MANAGER
+}
